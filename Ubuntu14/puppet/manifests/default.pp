eab7d28b0264044b3ff87f58f1fb7a2ad2e20077
Alvaro Almendros Gala <alvaroalmendros@me.com>
2015-12-04 21:17:01 +0100

    Create default.pp

---
+++ Ubuntu14/puppet/manifests/default.pp
@@ -0,0 +1,44 @@
+include git
+include '::mongodb::server'
+class { 'nodejs':
+  repo_url_suffix => 'node_0.12',
+}
+
+package { 'n':
+  ensure   => 'present',
+  provider => 'npm',
+}
+class basic_exec {
+  exec { 'install_n_and_node-5.1.1':
+    command => 'n 5.1.1',
+  }
+}
+
+package { 'express':
+  ensure   => 'present',
+  provider => 'npm',
+}
+package { 'yo':
+  ensure   => 'present',
+  provider => 'npm',
+}
+package { 'generator-angular ':
+  ensure   => 'present',
+  provider => 'npm',
+}
+package { 'nodemon':
+  ensure   => 'present',
+  provider => 'npm',
+}
+package { 'bower':
+  ensure   => 'present',
+  provider => 'npm',
+}
+package { 'gulp':
+  ensure   => 'present',
+  provider => 'npm',
+}
+package { 'grunt-cli':
+  ensure   => 'present',
+  provider => 'npm',
+}
