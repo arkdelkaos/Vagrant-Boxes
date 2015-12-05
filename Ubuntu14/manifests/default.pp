2e8d1317e61e0dba99143f3dcc42f43220b25017
Alvaro Almendros Gala <alvaroalmendros@me.com>
2015-12-05 17:10:16 +0100

    Intento de obligar a instalar node antes de lanzar npm

--- Ubuntu14/manifests/default.pp
+++ Ubuntu14/manifests/default.pp
@@ -1,45 +1,56 @@
 include git
 include '::mongodb::server'
 
-class { 'nodejs':
-  version => 'stable',
+class prepare {
+  class { 'nodejs':
+      version => 'stable',
+  }
 }
+include prepare
+package {'nodejs': ensure => present, require => Class['prepare'],}
 
+package { 'express':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
+package { 'yo':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
+package { 'generator-angular ':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
+package { 'nodemon':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
+package { 'bower':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
+package { 'gulp':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
+package { 'grunt-cli':
+  ensure   => 'present',
+  provider => 'npm',
+    require  => Package['nodejs'],
+}
 package { 'n':
   ensure   => 'present',
   provider => 'npm',
+    require  => Package['nodejs'],
 }
 class basic_exec {
   exec { 'install_n_and_node-5.1.1':
     command => 'n 5.1.1',
   }
-}
-
-package { 'express':
-  ensure   => 'present',
-  provider => 'npm',
-}
-package { 'yo':
-  ensure   => 'present',
-  provider => 'npm',
-}
-package { 'generator-angular ':
-  ensure   => 'present',
-  provider => 'npm',
-}
-package { 'nodemon':
-  ensure   => 'present',
-  provider => 'npm',
-}
-package { 'bower':
-  ensure   => 'present',
-  provider => 'npm',
-}
-package { 'gulp':
-  ensure   => 'present',
-  provider => 'npm',
-}
-package { 'grunt-cli':
-  ensure   => 'present',
-  provider => 'npm',
 }
